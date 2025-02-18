da848bc2e46c64297e2ed97cc13b1b2f92e5377a
ishan249 <249patelishan@gmail.com>
2025-02-18 22:05:38 +0530

    change from master

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more. changes done by dhruv
+        Click on the Vite and React logos to learn more. changes done by dhruv. ishan
       </p>
     </>
   )
